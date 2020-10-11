24d2e9ec2084a81388a0a529c6068242b41f3904
Kartik Choudhary <kartik123choudhary@gmail.com>
2020-10-11 12:33:04 +0530

    setting change to development

--- api/app/signals/celery.py
+++ api/app/signals/celery.py
@@ -2,7 +2,9 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals.settings.production')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals.settings.development')
+
 app = Celery('signals')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
